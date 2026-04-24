295895bd6cc537b7b7d4e4b81304f7024dbce7fe
jeromemaiquez <maiquez.jerome.ml@gmail.com>
2026-04-24 22:21:46 +0800

    Implemented main case study + test for Parcel and SpatialObject

---
+++ main_case_study.py
@@ -0,0 +1,38 @@
+import sys
+import os
+from pathlib import Path
+
+dir_models = Path().resolve() / "models"
+sys.path.append(os.path.abspath(dir_models))
+
+from building import Building
+from parcel import Parcel
+from road import Road
+from household import Household
+
+# Test for Parcel and SpatialObject
+parcel1_data = {
+    "parcel_id": "P1",
+    "geometry": {
+        "x": 121.055,
+        "y": 12.11,
+    },
+    "area": 109.5,
+    "zone": "Residential"
+}
+
+parcel2_data = {
+    "parcel_id": "P2",
+    "geometry": {
+        "x": 121.06,
+        "y": 12.11,
+    },
+    "area": 57.5,
+    "zone": "Residential"
+}
+
+parcel1 = Parcel(**parcel1_data)
+parcel2 = Parcel(**parcel2_data)
+
+print(parcel1.distance_to(parcel2))
+print(parcel1.intersects(parcel2, threshold=0.5))
