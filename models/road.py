f05fdb3c189f7c79c4f4cf0ba5a3b90a78d11f82
jeromemaiquez <maiquez.jerome.ml@gmail.com>
2026-04-24 21:42:39 +0800

    Implemented Road class

---
+++ models/road.py
@@ -0,0 +1,34 @@
+from spatial_object import SpatialObject
+
+class Road(SpatialObject):
+    """
+    Road is a spatial object with the following attributes:
+    - geometry
+    - length
+    - type
+
+    Relationship:
+    - adjacent to one or more parcels
+    """
+
+    def __init__(self, road_id: str, geometry: dict, length: float, road_type: str):
+        super().__init__(geometry)
+        self.road_id = road_id
+        self.length = length
+        self.road_type = road_type
+        self.adjacent_parcels = []
+
+    def get_length(self):
+        return self.length
+    
+    def add_adjacent_parcel(self, parcel):
+        if parcel not in self.adjacent_parcels:
+            self.adjacent_parcels.append(parcel)
+            parcel.add_adjacent_road(self)
+    
+    def describe(self):
+        parcel_ids = [p.parcel_id for p in self.adjacent_parcels]
+        return (
+            f"Road {self.road_id}: type={self.road_type}, "
+            f"length={self.length}, adjacent_parcels={parcel_ids}"
+        )
