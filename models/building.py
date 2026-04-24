7b1f2f688b14d57861066b90c0b9828b6c6a6472
jeromemaiquez <maiquez.jerome.ml@gmail.com>
2026-04-24 21:23:56 +0800

    Implemented Building class

---
+++ models/building.py
@@ -0,0 +1,43 @@
+from spatial_object import SpatialObject
+
+class Building(SpatialObject):
+    """
+    Building is a spatial object with the following attributes:
+    - geometry
+    - height
+    - usage
+
+    Relationship:
+    - located on one parcel
+    - may contain multiple households
+    """
+
+    def __init__(self, building_id, geometry, height, usage, parcel=None):
+        super().__init__(geometry)
+        self.building_id = building_id
+        self.height = height
+        self.usage = usage
+        self.parcel = parcel
+        self.households = []
+
+        if self.parcel is not None:
+            self.parcel.add_building(self)
+        
+    def get_height(self):
+        return self.height
+    
+    def assign_parcel(self, parcel):
+        self.parcel = parcel
+        self.parcel.add_building(self)
+
+    def add_household(self, household):
+        if household not in self.households:
+            self.households.append(household)
+    
+    def describe(self):
+        parcel_text = self.parcel.parcel_id if self.parcel else "None"
+        return (
+            f"Building {self.building_id}: usage={self.usage}, "
+            f"height={self.height}, parcel={parcel_text}, "
+            f"households={len(self.households)}" 
+        )
