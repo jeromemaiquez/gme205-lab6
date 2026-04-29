e78a1230ce2e77c9d7fa07b0a9031f754ae5436b
jeromemaiquez <maiquez.jerome.ml@gmail.com>
2026-04-29 21:45:21 +0800

    Basic implementation of Hub methods

--- own_models/hub.py
+++ own_models/hub.py
@@ -1,19 +1,23 @@
 from shapely import Point, Polygon
+from pathlib import Path
 
 class Hub:
     """
-    Base class for transport hubs (airport and seaport).
-    Contains ability to draw catchment (via ORS API isochrone)
-    and aggregate some variable as its size/"attraction".
+    Base class for transport hubs (airport and seaport),
+    which are modeled as point locations.
 
     Attributes:
     - name: str
-    - lon: longitude (x-coordinate)
-    - lat: latitude (y-coordinate)
-    - catchment: shapely.Polygon (default: None)
+        Name of the transport hub
+    - lon: float
+        Longitude of the transport hub
+    - lat: float
+        Latitude of the transport hub
+    - attraction: int/float
+        Attraction/size score of the transport hub
     """
 
-    def __init__(self, hub_name: str, lon: float, lat: float, catchment: Polygon | None = None):
+    def __init__(self, hub_name: str, lon: float, lat: float, attraction: float | None = None):
         if not (-180 <= lon <= 180):
             raise ValueError("Longitude must be between -180 and 180") 
         if not (-90 <= lat <= 180):
@@ -23,6 +27,25 @@ class Hub:
         self.lat = lat
         self.geometry = Point(lon, lat)
         self.hub_name = hub_name
-        self.catchment = catchment
+        self.metrics = {}
+        self.attraction = attraction
 
+    def set_attraction(self, value: int | float, override: bool = False) -> None:
+        """
+        Assigns a value as a score signifying "attraction"
+        or "size" of a given hub in the radiation model.
+
+        Attributes:
+        - value: int or float
+            Value to assign as the hub's attraction/size score
+        - override: bool
+            If True, overrides any existing attraction score
+        """
+        if (self.attraction) & (not override):
+            raise ValueError("Hub already has an attraction score. Set `override=True` if you wish to override")
+        
+        self.attraction = value
+    
+    def distance_to(self, other) -> float:
+        raise NotImplementedError("Must be implemented by Airport or Seaport subclass")
     
